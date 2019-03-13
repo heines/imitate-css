ad40a0d8149c34f3bf8250168afe4d762d07cc50
清水園子 <s-shimizu@tsumikiinc.com>
2019-03-13 15:23:31 +0900

    ハンバーガーリンク模擬

---
+++ src/js/modules/moves/closeMovesNav.js
@@ -0,0 +1,7 @@
+export default function() {
+  const nav = document.querySelector('.js-nav');
+  const navList = nav.classList;
+  if(navList.contains('is-open')) {
+    navList.replace('is-open', 'is-close');
+  }
+}
--- src/js/modules/pjax_with_preload/init/moves.js
+++ src/js/modules/pjax_with_preload/init/moves.js
@@ -1,5 +1,6 @@
 import loadContentImgs from '../../common/loadContentImgs';
 import openMovesNav from '../../moves/openMovesNav';
+import closeMovesNav from '../../moves/closeMovesNav';
 
 // initBeforeTransit method: before scrollManager.resize run.
 const initBeforeTransit = async (contents, modules) => {
@@ -7,8 +8,10 @@ const initBeforeTransit = async (contents, modules) => {
 
 // initAfterTransit method: after scrollManager.resize run.
 const initAfterTransit = (contents, modules) => {
-  const navButton = contents.querySelector(".js-nav-button");
-  navButton.addEventListener("click", openMovesNav, false);
+  const navButtonOpen = contents.querySelector(".js-nav-button-open");
+  navButtonOpen.addEventListener("click", openMovesNav, false);
+  const navButtonClose = contents.querySelector(".js-nav-button-close");
+  navButtonClose.addEventListener("click", closeMovesNav, false);
 };
 
 // clear any variables.
